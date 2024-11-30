9952c74122881f8870f1c7c8a539e48a65146bcf
AIbanezWA <68170804+AIbanezWA@users.noreply.github.com>
2024-11-30 13:11:50 -0500

    Create cost.py
    Create cost.py

---
+++ OpenAI/cost.py
@@ -0,0 +1,73 @@
+# DEMO #2
+# Reference: https://openai.com/pricing
+
+
+from openai import OpenAI
+import getpass, os
+
+# Se define una variable de entorno para el API key.
+os.environ["OPENAI_API_KEY"] = getpass.getpass("Ingresa tu API Key de OpenAI : ")
+
+# Se define el modelo de lenguaje que se va a utilizar.
+model_openai = "gpt-4o"
+
+# Ya no es necesario utilizar la variable del API key como parámetro en la clase OpenAI. Internamente lo de la variable de entorno
+client = OpenAI()
+
+# Se define una función con 2 argumentos: uso (input/output) y el modelo.
+def openai_api_calculate_cost(usage, model):
+
+    pricing = {
+        'gpt-3.5-turbo-0125': {
+            'prompt': 0.0005,
+            'completion': 0.0015
+        },
+        'gpt-4o': {
+            'prompt': 0.005,
+            'completion': 0.015
+        }
+    }
+
+    try:
+        model_pricing = pricing[model]
+    except KeyError:
+        raise ValueError("Modelo inválido")
+
+    prompt_cost = usage['prompt_tokens'] * model_pricing['prompt'] / 1000
+    completion_cost = usage['completion_tokens'] * model_pricing['completion'] / 1000
+
+    total_cost = prompt_cost + completion_cost
+    print(f"\nTokens usados:  {usage['prompt_tokens']:,} prompt + {usage['completion_tokens']:,} completion = {usage['total_tokens']:,} tokens")
+    print(f"Total costo ({model}): ${total_cost:4f}\n")
+
+    return total_cost
+
+# Define el mensaje de entrada para el chat
+prompt = "dame el código para usar la API de OpenAI con Python"
+message_input = {
+    'messages': [
+        {'role': 'system', 'content': 'Eres un asistente virtual'},
+        {'role': 'user', 'content': prompt}
+    ]
+}
+
+# Realiza una solicitud a chatGPT con la API de OpenAI
+response = client.chat.completions.create(
+    model = model_openai,
+    messages = message_input['messages'],
+    temperature = 0, #Si está más cercano a 1, es posible que tenga alucinaciones.
+    n = 1, #Número de respuestas
+    max_tokens = 400
+)
+
+result = response.choices[0].message.content
+print(result)
+
+# Se declara un array para capturar la respuesta
+data_usage = {
+    "completion_tokens" : response.usage.completion_tokens, 
+    "prompt_tokens" :  response.usage.prompt_tokens,
+    "total_tokens" : response.usage.total_tokens
+}
+
+openai_api_calculate_cost(data_usage, model_openai)
